dc037684a11e6b25736d7211e56692f860b4cb7f
JRonca <jrfr21112000@gmail.com>
2025-01-27 07:37:20 -0300

    feat: finish loops

--- loops/main.go
+++ loops/main.go
@@ -8,4 +8,14 @@ func main() {
     res++
   }
   fmt.Println(res)
+
+  // For range
+
+  arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+  for i, elem := range arr {
+    fmt.Println(i, elem)
+  }
+  for i := range 10 {
+    fmt.Println(i)
+  }
 }
