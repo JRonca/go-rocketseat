63dffd04db9c0ae46a69ba4d359b3eeda808fa12
JRonca <jrfr21112000@gmail.com>
2025-01-28 07:15:37 -0300

    if condition

---
+++ if/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+  y := 8
+  if y < 10 {
+    fmt.Println("ok")
+  }
+
+  if x:= math.Sqrt(4); x < 1 {
+    fmt.Println(x)
+  } else if x > 0 {
+    fmt.Println("ok ok")
+  } else {
+    fmt.Println("no")
+  }
+}
