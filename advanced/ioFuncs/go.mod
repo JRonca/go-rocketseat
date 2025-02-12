7cdc5ccbd30c785ade73c1f003de242d54f7d71d
JRonca <jrfr21112000@gmail.com>
2025-02-12 19:49:24 -0300

    Feat: io funcs

--- advanced/ioFuncs/go.mod
+++ advanced/ioFuncs/go.mod
@@ -1,3 +1,3 @@
-module writerWrapping
+module ioFuncs
 
 go 1.23.5
--- advanced/ioFuncs/main.go
+++ advanced/ioFuncs/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"strings"
 )
@@ -9,5 +10,20 @@ func main() {
 	str := "Hello, world\n"
 	reader := strings.NewReader(str)
 
-	io.ReadAll(reader)
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+	fmt.Print(string(data))
+
+	str2 := "abc"
+	reader2 := strings.NewReader(str2)
+
+	dataFull, errFull := io.ReadFull(reader2, make([]byte, 3))
+	if errFull != nil {
+		fmt.Println("Error reading: ", errFull)
+		return
+	}
+	fmt.Print(dataFull)
 }
